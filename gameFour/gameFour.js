1228f17e13136b4100561f49c8af3a2739ca0316
Joe May <joemaybooking@gmail.com>
2020-07-15 15:41:28 -0700

    added game4 skeleton

---
+++ gameFour/gameFour.js
@@ -0,0 +1,184 @@
+
+$('.win').hide();
+$('.lose').hide();
+
+
+////snowball throw
+$('body').click(function(e) {
+  var parentOffset = $(this).parent().offset();
+   var relX = e.pageX - parentOffset.left;
+   var relY = e.pageY - parentOffset.top;
+  $(".snowball").animate({
+      width: '10px',
+      left: relX,
+      top: relY
+  }, 200, function() {
+      $(".snowball").removeAttr('style');
+  });
+});
+
+const answerset = [
+  { problem: "3 + 2 =", answer: "5"},
+  { problem: "6 + 1 =", answer: "7"},
+  { problem: "4 + 4 =", answer: "8"},
+  { problem: "1 + 2 =", answer: "3"},
+  { problem: "3 + 1 =", answer: "4"},
+  { problem: "7 + 3 =", answer: "10"},
+  { problem: "6 + 3 =", answer: "9"},
+  { problem: "2 + 4 =", answer: "6"},
+  
+]
+
+$('.start').on('click',function() {
+$('.start').hide();
+$('.win').hide();
+$('.lose').hide();
+
+
+
+// $(document).click(function(e) {
+//   mouseX = e.pageX;
+//   $('.dart').css("left", e.pageX);
+// });
+
+var currentGameArray = [];
+// var playerLives = 0;
+
+
+answerset.forEach(function(questions) {
+  currentGameArray.push(questions)
+})
+
+
+  var randomProblemSelector = Math.floor((Math.random() * currentGameArray.length));
+  var selectedProblem = currentGameArray[randomProblemSelector].problem;
+  var theAnswer = currentGameArray[randomProblemSelector].answer;
+
+console.log(currentGameArray);
+
+  $.each(currentGameArray, function(index,value){
+    $(".answers").append("<div class='house'><div class='iglooWrapper "+'a'+index+"'><img src='../StudyPup_assets/ice_block_1.png' class='igloo'><img src='../StudyPup_assets/penguin_transparent.gif' class='penguin hide'><div class='answer'>" + value.answer + "</div></div></div>");
+    console.log("index: " + index + " problem: " + value.problem + " answer: " + value.answer );
+  });
+
+ 
+  var counter = 1000;
+ 
+
+  
+  var interval = setInterval(function() {
+    counter--;
+    // Display 'counter' wherever you want to display it.
+    if (counter <= 0 && currentGameArray.length > 0) {
+         clearInterval(interval);
+          // $('#time').text(counter);
+          console.log("you lose");
+          $('.lose').show();
+          $(".reset").html(" ");
+          $(".start").show();
+          
+        return;
+    }else{
+      $('#time').text(counter);
+      
+      console.log("Timer --> " + counter);
+    }
+}, 1000);
+
+  
+
+  // display the selected problem on the screen
+  
+  var currentEquation = function () {
+    var problemHtml = $('<p></p>').html(selectedProblem);
+    return $(".problem").append(problemHtml);
+      
+  }
+ 
+  currentEquation();
+  
+
+  // when answer is clicked on
+  $('.iglooWrapper').on('click',function() {
+    var clickedAnswer = $(this).text();
+    
+    if (clickedAnswer === theAnswer) {
+      console.log("Correct!");
+      $(this).find('.igloo').fadeOut(10);
+      $(this).find('.answer').fadeOut(10, function() {
+        $(this).parent('.iglooWrapper').prepend("<img src='' class='penguin'>");
+        console.log(currentGameArray.length);
+        var penguin = $(this).parent().find('.penguin');
+        penguin.attr("src", "../StudyPup_assets/Net_Scoop_Ice_1.gif");
+        
+        
+          currentGameArray.splice(randomProblemSelector,1);
+          console.log(currentGameArray.length);
+          winningCheck();
+          generateNextTurn();
+          console.log($(this).parent());
+          setTimeout(function() {
+            penguin.attr("src", "").remove();
+        }, 1000)
+      });
+
+   
+
+    } else {
+     
+    };
+  });
+    // if (playerLives === 3) {
+    //   $('.lose').show();
+    //   $(".reset").html(" ");
+    //   $(".start").show();
+    //   // generateHearts();
+      
+      
+
+      
+  //   }
+  
+
+
+  function generateNextTurn() {
+    if (currentGameArray.length === 0){
+    return;
+    }
+    randomProblemSelector = Math.floor((Math.random() * currentGameArray.length));
+    selectedProblem = currentGameArray[randomProblemSelector].problem;
+    theAnswer = currentGameArray[randomProblemSelector].answer;
+    //$("p").first().html(selectedProblem);
+
+    var problemHtml = $('<p></p>').html(selectedProblem);
+    $(".problem").append(problemHtml);
+    
+  }
+  
+
+  function winningCheck() {
+  
+  if (currentGameArray.length > 0){
+console.log('keep playing');
+  } else {
+    $('.win').show();
+    $(".reset").html(" ");
+    $(".start").show();
+    $('.tryAgain').show();
+    clearInterval(interval);
+    console.log('you win')
+    // generateHearts();
+    
+   
+    
+  };
+}
+  
+}); 
+
+
+// var generateHearts = function () {
+// var heartsLoad = $('<li></li>').html('<img class=heart src="https://i.imgur.com/MQ88P97.png" />');
+// $("ul").append(heartsLoad);
+// console.log("generating!");
+// }
